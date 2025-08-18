f065755513361dca2f1a805732e6f08d743568b1
Marco Silveira <marcoan.silveira@procempa.com.br>
2025-08-18 17:35:21 -0300

    Desafio nível novato - versão final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
+/* Desafio Super Trunfo - Países
+ Tema 1 - Cadastro das Cartas
+ OBS: Nível Novato do desafio é focado apenas no cadastro das cartas, 
+      utilizando as funções scanf para ler os dados e printf para exibi-los.
+*/ 
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    // Definição das variáveis separadas para cada atributo da cidade.
     char  estado_carta1, estado_carta2;
     char  codigoCarta_carta1[4],codigoCarta_carta2[4]; // precisa de 4 posições: "A01" + '\0'
     char  nomeCidade_carta1[50], nomeCidade_carta2[50];
@@ -14,12 +17,12 @@ int main() {
     float pib_carta1, pib_carta2;
     int   numPontosTuristicos_carta1, numPontosTuristicos_carta2;
    
-    // Cadastro das Cartas:
     /*
-    INFORMANDO OS DADOS DA CARTA 1
+    Cadastrar Cartas - Cidade 1:
     */
-    printf("Informe os dados da CARTA 1\n");
-    printf("---------------------------\n");
+    printf("--------------------------------------\n");
+    printf("Informe os dados da CARTA 1 (Cidade 1)\n");
+    printf("--------------------------------------\n");
 
     printf("Estado: (Uma letra de 'A' a 'H'): ");
     scanf(" %c", &estado_carta1);   // espaço antes do "%c" para ignora o enter, tabs e espaços
@@ -45,12 +48,25 @@ int main() {
     printf("Número de Pontos Turísticos (A quantidade de pontos turísticos na cidade): "); 
     scanf("%d", &numPontosTuristicos_carta1);
 
+    // Exibição dos Dados da Carta 1
+    printf("\n==> RESULTADO\n");
+    printf("CARTA 1 \nEstado: %c \nCódigo: %s \nNome da cidade: %s \nPopulação: %d \nÁrea: %.2f km² \nPIB: %.2f bilhões de reais \nNúmero de pontos turísticos: %d", 
+        estado_carta1, 
+        codigoCarta_carta1,
+        nomeCidade_carta1,
+        populacao_carta1,
+        areaKm_carta1,
+        pib_carta1,
+        numPontosTuristicos_carta1
+    );
+
     /*
-    INFORMANDO OS DADOS DA CARTA 2
+    Cadastrar Cartas - Cidade 2:
     */
-    printf("\n");
-    printf("Informe os dados da CARTA 2\n");
-    printf("---------------------------\n");
+    printf("\n\n");
+    printf("--------------------------------------\n");
+    printf("Informe os dados da CARTA 2 (Cidade 2)\n");
+    printf("--------------------------------------\n");
 
     printf("Estado: (Uma letra de 'A' a 'H'): ");
     scanf(" %c", &estado_carta2);   // espaço antes do "%c" para ignora o enter, tabs e espaços
@@ -76,19 +92,9 @@ int main() {
     printf("Número de Pontos Turísticos (A quantidade de pontos turísticos na cidade): "); 
     scanf("%d", &numPontosTuristicos_carta2);
     
-    // Exibição dos Dados das Cartas:
-     printf("\n==> RESULTADO <==\n");
-    printf("CARTA 1 \nEstado: %c \nCódigo: %s \nNome da Cidade: %s \nPopulação: %d \nÁrea: %.2f km² \nPIB: %.2f bilhões de reais: \nNúmero de pontos turísticos: %d\n\n", 
-        estado_carta1, 
-        codigoCarta_carta1,
-        nomeCidade_carta1,
-        populacao_carta1,
-        areaKm_carta1,
-        pib_carta1,
-        numPontosTuristicos_carta1
-    );
-
-    printf("CARTA 2 \nEstado: %c \nCódigo: %s \nNome da Cidade: %s \nPopulação: %d \nÁrea: %.2f km² \nPIB: %.2f bilhões de reais: \nNúmero de pontos turísticos: %d\n", 
+    // Exibição dos Dados da Carta 2
+    printf("\n==> RESULTADO\n");
+    printf("CARTA 2 \nEstado: %c \nCódigo: %s \nNome da cidade: %s \nPopulação: %d \nÁrea: %.2f km² \nPIB: %.2f bilhões de reais \nNúmero de pontos turísticos: %d", 
         estado_carta2, 
         codigoCarta_carta2,
         nomeCidade_carta2,
